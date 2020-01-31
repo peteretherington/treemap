7daeb6124fc0ae61243469bfe35dd051c691a1e3
Peter Etherington <pjg.etherington@gmail.com>
2020-01-30 19:46:56 -0500

    All tests passing

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const movieSales =
 
 const treeWidth = 960,
 	treeHeight = 570,
-	legendHeight = 10,
-	legendWidth = 500;
+	legendHeight = 80,
+	legendWidth = 960;
 
 const treeSVG = d3.select('#treeMap').attr('viewBox', [0, 0, treeWidth, treeHeight]);
-const legendSVG = d3.select('#legend');
+const legendSVG = d3.select('#legend').attr('viewBox', [0, 0, legendWidth, legendHeight]);
+const tooltip = d3.select('#tooltip');
 
 const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
@@ -22,6 +23,8 @@ const treemap = d3
 
 // =========================
 const visualize = data => {
+	const genreArray = data.children.reduce((acc, cur) => [...acc, cur.name], []);
+
 	const nodes = treemap(
 		d3
 			.hierarchy(data)
@@ -34,7 +37,21 @@ const visualize = data => {
 		.data(nodes.leaves())
 		.enter()
 		.append('g')
-		.attr('transform', d => `translate(${d.x0}, ${d.y0})`);
+		.attr('transform', d => `translate(${d.x0}, ${d.y0})`)
+		.on('mouseover', d => {
+			const info = `
+				<span>Title: ${d.data.name}</span>
+				<br />
+				<span>Box Office: ${d3.format('$,')(d.data.value)}</span>
+			`;
+			tooltip
+				.attr('data-value', d.data.value)
+				.style('opacity', 1)
+				.style('top', `${d3.event.pageY - 75}px`)
+				.style('left', `${d3.event.pageX + 10}px`)
+				.html(info);
+		})
+		.on('mouseout', () => tooltip.style('opacity', 0));
 
 	// Tile properties
 	leaf
@@ -63,17 +80,41 @@ const visualize = data => {
 		.enter()
 		.append('tspan')
 		.attr('x', 3)
-		.attr('y', (d, i) => 13 + i * 10)
+		.attr('y', (d, i) => i * 10 + 10)
+		.attr('fill', 'white')
+		.text(d => d);
+
+	// Legend
+	const legend = legendSVG
+		.selectAll('g')
+		.data(genreArray)
+		.enter()
+		.append('g');
+
+	const legendTile = 20;
+
+	legend
+		.append('rect')
+		.attr('class', 'legend-item')
+		.attr('width', legendTile)
+		.attr('height', legendTile)
+		.attr('x', (d, i) => (i < 4 ? i * legendTile * 5 : (i - 4) * legendTile * 5))
+		.attr('y', (d, i) => (i < 4 ? 0 : legendTile + 10))
+		.attr('fill', d => colorScale(d));
+
+	legend
+		.append('text')
+		.attr('x', (d, i) =>
+			i < 4 ? i * legendTile * 5 + legendTile * 1.25 : (i - 4) * legendTile * 5 + legendTile * 1.25
+		)
+		.attr('y', (d, i) => (i < 4 ? legendTile / 1.5 : legendTile * 2.25))
+		.style('fill', 'white')
 		.text(d => d);
 };
 
 d3.json(movieSales, visualize);
 
 /**
- * Chart
- * - My legend should have <rect> elements with a corresponding class="legend-item".
- * - The <rect> elements in the legend should use at least 2 different fill colors.
- *
  * Tool Tip
  * - I can mouse over an area and see a tooltip with a corresponding id="tooltip" which displays more information about the area.
  * - My tooltip should have a "data-value" property that corresponds to the "data-value" of the active area.
